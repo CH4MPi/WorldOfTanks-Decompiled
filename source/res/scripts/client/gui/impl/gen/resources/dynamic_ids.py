ca9150f30691292d2451be99ef7ee717a6ef438a
CHAMPi <no@one.com>
2021-04-04 09:25:02 +0200

    1.12.0.0 EU: Updated #762

--- source/res/scripts/client/gui/impl/gen/resources/dynamic_ids.py
+++ source/res/scripts/client/gui/impl/gen/resources/dynamic_ids.py
@@ -7,45 +7,45 @@ class DynamicIds(DynAccessor):
 
     class _dialog_window(DynAccessor):
         __slots__ = ()
-        bottom_content = DynAccessor(54350)
-        balance_content = DynAccessor(54351)
+        bottom_content = DynAccessor(54376)
+        balance_content = DynAccessor(54377)
 
     dialog_window = _dialog_window()
 
     class _tooltip(DynAccessor):
         __slots__ = ()
-        normal_content = DynAccessor(54352)
-        advanced_content = DynAccessor(54353)
+        normal_content = DynAccessor(54378)
+        advanced_content = DynAccessor(54379)
 
     tooltip = _tooltip()
 
     class _blueprint_screen(DynAccessor):
         __slots__ = ()
-        balance_content = DynAccessor(54354)
+        balance_content = DynAccessor(54380)
 
     blueprint_screen = _blueprint_screen()
 
     class _crew_books_buy_dialog(DynAccessor):
         __slots__ = ()
-        balance_content = DynAccessor(54355)
+        balance_content = DynAccessor(54381)
 
     crew_books_buy_dialog = _crew_books_buy_dialog()
 
     class _prem_dashboard(DynAccessor):
         __slots__ = ()
-        header = DynAccessor(54356)
-        premium_card = DynAccessor(54357)
-        double_xp_card = DynAccessor(54358)
-        piggy_bank_card = DynAccessor(54359)
-        dog_tags_card = DynAccessor(54360)
-        premium_quests_card = DynAccessor(54361)
-        maps_black_list_card = DynAccessor(54362)
+        header = DynAccessor(54382)
+        premium_card = DynAccessor(54383)
+        double_xp_card = DynAccessor(54384)
+        piggy_bank_card = DynAccessor(54385)
+        dog_tags_card = DynAccessor(54386)
+        premium_quests_card = DynAccessor(54387)
+        maps_black_list_card = DynAccessor(54388)
 
     prem_dashboard = _prem_dashboard()
 
     class _demo_window(DynAccessor):
         __slots__ = ()
-        image_props = DynAccessor(54363)
-        bottom_panel = DynAccessor(54364)
+        image_props = DynAccessor(54389)
+        bottom_panel = DynAccessor(54390)
 
     demo_window = _demo_window()
--- source/res/scripts/client/gui/impl/gen/resources/videos.py
+++ source/res/scripts/client/gui/impl/gen/resources/videos.py
@@ -4,52 +4,52 @@ from gui.impl.gen_utils import DynAccessor
 
 class Videos(DynAccessor):
     __slots__ = ()
-    _bootcampLesson1 = DynAccessor(54326)
-    _bootcampLesson2 = DynAccessor(54327)
-    _bootcampLesson3_1 = DynAccessor(54328)
-    _bootcampLesson3_2 = DynAccessor(54329)
-    _bootcampLesson4 = DynAccessor(54330)
-    _bootcampOutro = DynAccessor(54331)
-    _login = DynAccessor(54332)
-    _tutorialInitial = DynAccessor(54333)
-    _tutorialInitialLoop = DynAccessor(54334)
+    _bootcampLesson1 = DynAccessor(54352)
+    _bootcampLesson2 = DynAccessor(54353)
+    _bootcampLesson3_1 = DynAccessor(54354)
+    _bootcampLesson3_2 = DynAccessor(54355)
+    _bootcampLesson4 = DynAccessor(54356)
+    _bootcampOutro = DynAccessor(54357)
+    _login = DynAccessor(54358)
+    _tutorialInitial = DynAccessor(54359)
+    _tutorialInitialLoop = DynAccessor(54360)
 
     class _battle_pass(DynAccessor):
         __slots__ = ()
-        c_65612_2 = DynAccessor(54335)
-        c_65612_3 = DynAccessor(54336)
-        c_65612_4 = DynAccessor(54337)
-        c_65868_2 = DynAccessor(54338)
-        c_65868_3 = DynAccessor(54339)
-        c_65868_4 = DynAccessor(54340)
-        c_66124_2 = DynAccessor(54341)
-        c_66124_3 = DynAccessor(54342)
-        c_66124_4 = DynAccessor(54343)
+        c_65612_2 = DynAccessor(54361)
+        c_65612_3 = DynAccessor(54362)
+        c_65612_4 = DynAccessor(54363)
+        c_65868_2 = DynAccessor(54364)
+        c_65868_3 = DynAccessor(54365)
+        c_65868_4 = DynAccessor(54366)
+        c_66124_2 = DynAccessor(54367)
+        c_66124_3 = DynAccessor(54368)
+        c_66124_4 = DynAccessor(54369)
 
     battle_pass = _battle_pass()
 
     class _development(DynAccessor):
         __slots__ = ()
-        example = DynAccessor(54344)
-        example_2 = DynAccessor(54345)
+        example = DynAccessor(54370)
+        example_2 = DynAccessor(54371)
 
     development = _development()
 
     class _platoon(DynAccessor):
         __slots__ = ()
-        VoiceChat = DynAccessor(54346)
+        VoiceChat = DynAccessor(54372)
 
     platoon = _platoon()
 
     class _startup(DynAccessor):
         __slots__ = ()
-        ESRB = DynAccessor(54347)
-        Logo_All = DynAccessor(54348)
+        ESRB = DynAccessor(54373)
+        Logo_All = DynAccessor(54374)
 
     startup = _startup()
 
     class _vehicle(DynAccessor):
         __slots__ = ()
-        A122_TS_5 = DynAccessor(54349)
+        A122_TS_5 = DynAccessor(54375)
 
     vehicle = _vehicle()
